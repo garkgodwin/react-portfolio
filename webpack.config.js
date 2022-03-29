35521821c40e3d2d6d16ea707ce326288089c32a
gark <gduque@geniihut.com>
2022-03-30 00:34:06 +0800

    Refractor: added webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,60 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  loaders: [
-    {
-      test: /\.(ico|png|eot|svg|ttf|woff|woff2)$/,
-      loader: "file?name=[name]-[hash:6].[ext]",
+module.exports = function (_env, argv) {
+  const isProduction = argv.mode === "production";
+  const isDevelopment = !isProduction;
+
+  return {
+    devtool: isDevelopment && "cheap-module-source-map",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "assets/js/[name].[contenthash:8].js",
+      publicPath: "/",
     },
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? "production" : "development",
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: "assets/css/[name].[contenthash:8].css",
+          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
+        }),
+    ].filter(Boolean),
+  };
 };
